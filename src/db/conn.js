f449065a22e340af641e20dfb584a83cfa38cc03
Omkar Kadam <okadam@deloitte.com>
2021-02-17 01:17:05 +0530

    initial commit1

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/users-db',{
+//mongoose.connect('mongodb://localhost:27017/users-db',{
+    mongoose.connect('mongodb+srv://user:Omkar@123@cluster0.gmwag.mongodb.net/user-db?retryWrites=true&w=majority',{
     useCreateIndex : true,
     useNewUrlParser : true,
     useUnifiedTopology : true,
