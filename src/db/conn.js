eae02531dedd8e1f8b4fa5c8ca63a0c86648f6f6
Omkar Kadam <okadam@deloitte.com>
2021-02-17 10:51:50 +0530

    removed mongoose client...

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,28 +2,28 @@ const mongoose = require('mongoose');
 
 
 //mongoose.connect('mongodb://localhost:27017/users-db',{
-//     mongoose.connect('mongodb+srv://user:Omkar@123@cluster0.gmwag.mongodb.net/user-db?retryWrites=true&w=majority',{
-//     useCreateIndex : true,
-//     useNewUrlParser : true,
-//     useUnifiedTopology : true,
-//     useFindAndModify : false
-// }).then(()=>{
-//     console.log('connection established for mongo db');
-// }).catch((e)=>{
-//     console.log(e);
-// })
+    mongoose.connect('mongodb+srv://user:Omkar@123@cluster0.gmwag.mongodb.net/user-db?retryWrites=true&w=majority',{
+    useCreateIndex : true,
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useFindAndModify : false
+}).then(()=>{
+    console.log('connection established for mongo db');
+}).catch((e)=>{
+    console.log(e);
+})
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://user:Omkar@123@cluster0.gmwag.mongodb.net/user-db?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useCreateIndex : true,
-        useNewUrlParser : true,
-        useUnifiedTopology : true,
-        useFindAndModify : false
- });
-client.connect(err => {
-  const collection = client.db("user-db").collection("users");
-  console.log(collection);
-  console.log(err);
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user:Omkar@123@cluster0.gmwag.mongodb.net/user-db?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useCreateIndex : true,
+//         useNewUrlParser : true,
+//         useUnifiedTopology : true,
+//         useFindAndModify : false
+//  });
+// client.connect(err => {
+//   const collection = client.db("user-db").collection("users");
+//   console.log(collection);
+//   console.log(err);
+//   client.close();
+// });
